5ae585e21c57988bf5183186b9a38dfd4f1a7188
Yash Jahit <yashjahit@gmail.com>
2019-12-01 15:16:57 +0000

    Added and about plugin dialog

---
+++ src/main/kotlin/com/tfc/ulht/AboutPlugin.kt
@@ -0,0 +1,52 @@
+package com.tfc.ulht
+
+/**
+ *  Open authors LinkedIn webpage
+ */
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAware
+import java.net.URI
+import javax.swing.*
+
+
+class AboutPlugin: DumbAware, AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+        val text = "This plugin is designed to help students submit their project\n" +
+                "on the Drop Project platform without needing to create a zip.\n\n\n" +
+                "Plugin made by: Yash Jahit - https://github.com/yashjahit-21705201/Plugin-Drop-Project"
+
+        JOptionPane.showMessageDialog(
+            null,
+            text,
+            "About",
+            JOptionPane.INFORMATION_MESSAGE,
+            imageIconCreate())
+
+    }
+
+    fun openURI() {
+        val githubLink = "https://github.com/yashjahit-21705201/Plugin-Drop-Project"
+        val url = URI(githubLink)
+        try {
+            val desktop = java.awt.Desktop.getDesktop()
+            desktop.browse(url)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun imageIconCreate(): Icon? {
+
+        val pluginLogo = ImageIcon(this.javaClass.classLoader.getResource("images/plugin_dp_logo_smaller.png")).image
+        val icon = ImageIcon(pluginLogo)
+
+        return icon
+    }
+
+}
+
+
--- src/main/kotlin/com/tfc/ulht/exampleDialogs/ShowUserWritedMessage.kt
+++ src/main/kotlin/com/tfc/ulht/exampleDialogs/ShowUserWritedMessage.kt
@@ -1,4 +1,4 @@
-package com.tfc.ulht
+package com.tfc.ulht.exampleDialogs
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
